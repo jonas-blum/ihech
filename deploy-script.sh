c366ac5ac40435c43529dced53f13f7139ad6e32
Jonas Blum <jonas.blum2@uzh.ch>
2024-06-09 13:51:01 +0200

    adding a sleep

--- deploy-script.sh
+++ deploy-script.sh
@@ -20,6 +20,8 @@ if [ "$LOCAL" != "$REMOTE" ]; then
     docker build -t $FRONTEND_IMAGE ./frontend --build-arg VITE_API_URL=$VITE_API_URL
     docker push $FRONTEND_IMAGE
 
+    sleep 5
+
     /usr/local/bin/kubectl set image deployment/ihech-backend ihech-backend=$BACKEND_IMAGE
     /usr/local/bin/kubectl set image deployment/ihech-frontend ihech-frontend=$FRONTEND_IMAGE
 

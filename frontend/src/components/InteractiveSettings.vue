3d339885fca42fe460debc437d6c6aaa03aecac1
michi <michi.blum@hotmail.com>
2024-11-19 00:37:51 +0100

    interactive settings are now working

--- frontend/src/components/InteractiveSettings.vue
+++ frontend/src/components/InteractiveSettings.vue
@@ -3,8 +3,9 @@ import { useHeatmapStore } from '@stores/heatmapStore'
 import { useHeatmapLayoutStore } from '@stores/heatmapLayoutStore'
 import { Icon } from '@iconify/vue'
 
+const heatmapStore = useHeatmapStore()
+
 const itemGroupingOptions = [
-  { label: 'No Grouping', value: 'none' },
   { label: 'Kanton', value: 'kanton' },
   { label: 'District', value: 'district' },
   { label: 'Language Region', value: 'language' },
@@ -26,33 +27,78 @@ const kOptions = [
   { label: 'k=9', value: 9 },
   { label: 'k=10', value: 10 },
 ]
+
+async function updateClusterSize(event: Event) {
+  if (!(event.target instanceof HTMLSelectElement)) {
+    console.error('Event target is not an HTMLSelectElement:', event.target)
+    return
+  }
+  const size = event.target.value
+  heatmapStore.setClusterSize(parseInt(size, 10))
+  heatmapStore.setIsOutOfSync(true)
+}
+
+async function updateClusterByCollections(event: Event) {
+  if (!(event.target instanceof HTMLInputElement)) {
+    console.error('Event target is not an HTMLInputElement:', event.target)
+    return
+  }
+  heatmapStore.setClusterByCollections(event.target.checked)
+  heatmapStore.setIsOutOfSync(true)
+}
+
+async function updateClusterAfterDimRed() {
+  heatmapStore.setClusterAfterDimRed(!heatmapStore.getActiveDataTable?.clusterAfterDimRed)
+  heatmapStore.setIsOutOfSync(true)
+}
 </script>
 
 <template>
   <div class="text-md">
     <p>You are exploring the age distribution of residents across municipalities in Switzerland.</p>
     <p>
-      Municipalities are grouped by
-      <select class="select select-bordered select-xs w-min mx-1">
-        <option v-for="option in itemGroupingOptions" :value="option.value">
+      Municipalities are
+      <span v-if="!heatmapStore.getActiveDataTable?.clusterByCollections">not</span> grouped (
+      <input
+        @click="updateClusterByCollections"
+        type="checkbox"
+        class="toggle toggle-xs translate-y-[4px]"
+        :checked="heatmapStore.getActiveDataTable?.clusterByCollections"
+      />
+      )
+      <span v-if="heatmapStore.getActiveDataTable?.clusterByCollections">
+        by
+        <select class="select select-bordered select-xs w-min mx-1">
+          <option v-for="option in itemGroupingOptions" :value="option.value">
+            {{ option.label }}
+          </option>
+        </select>
+      </span>
+      and recursively clustered using the k-means algorithm with
+      <select
+        @change="updateClusterSize($event)"
+        class="select select-bordered select-xs w-min mx-1"
+      >
+        <option
+          v-for="option in kOptions"
+          :value="option.value"
+          :selected="heatmapStore.getActiveDataTable?.clusterSize === option.value"
+        >
           {{ option.label }}
         </option>
       </select>
-      and recursively clustered using the k-means algorithm with
-      <select class="select select-bordered select-xs w-min mx-1">
-        <option v-for="option in kOptions" :value="option.value">{{ option.label }}</option>
-      </select>
-      on the 
-      <select class="select select-bordered select-xs w-min mx-1">
-        <option value="hd">high-dimensional</option>
-        <option value="ld">dimensionality reduced</option>
+      on the
+      <select @change="updateClusterAfterDimRed" class="select select-bordered select-xs w-min mx-1">
+        <option :selected="!heatmapStore.getActiveDataTable?.clusterAfterDimRed">high-dimensional</option>
+        <option :selected="heatmapStore.getActiveDataTable?.clusterAfterDimRed">dimensionality reduced</option>
       </select>
       data.
     </p>
     <p>
+      <!-- TODO: toggle to disable grouping of attributes (analogously to clusterByCollections) -->
       Age groups are grouped by
       <select class="select select-bordered select-xs w-min mx-1">
-        <option v-for="option in attributeGroupingOptions" :value="option.value">
+        <option v-for="option in attributeGroupingOptions" :value="option.value" @change="">
           {{ option.label }}
         </option>
       </select>

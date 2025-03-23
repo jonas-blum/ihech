383111af1cc8d9f4ba26eee78d8078519b4b812f
Michael Blum <michi.blum@hotmail.com>
2025-03-23 11:47:25 +0100

    cell hover snippts

--- frontend/src/components/Heatmap.vue
+++ frontend/src/components/Heatmap.vue
@@ -621,21 +621,21 @@ onMounted(async () => {
     <!-- [value] [%] -->
     <div class="inline">
       <span class="font-bold mx-[3px]">
-        {{ mainStore.hoveredPixiHeatmapCell?.value }} tags
+        {{ mainStore.hoveredPixiHeatmapCell?.value }} 
       </span>
-      <!-- <span>{{ mainStore.getActiveDataTable?.cellHoverTextSnippet3 }}</span> -->
+      <span>{{ mainStore.getActiveDataTable?.cellHoverTextSnippet3 }}</span>
     </div>
 
     <!-- single attribute -->
-    <!-- <div class="inline" v-if="(mainStore.highlightedColumn instanceof AttributeColumn)">
+    <div class="inline" v-if="(mainStore.highlightedColumn instanceof AttributeColumn)">
       <span class="mx-[3px]">the {{ mainStore.getActiveDataTable?.attributeNameSingular }}</span>
       <span class="font-bold mx-[3px]">{{ mainStore.highlightedColumn?.getName() }}</span>
-    </div> -->
+    </div>
     <!-- attribute aggregate  -->
-    <!-- <div class="inline" v-else>
+    <div class="inline" v-else>
       <span class="mx-[3px]">this group of {{ mainStore.getActiveDataTable?.attributeNamePlural }}</span>
       <span class="font-bold mx-[3px]">({{ mainStore.highlightedColumn?.getName() }})</span>
-    </div> -->
+    </div>
   </div>
 
   <!-- Attribute Tooltip -->
